6f665ba323d111ff81aa49ed4a9c62ce6081856c
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-06-21 10:22:01 +0600

    some

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Context/UserContext";
+import { useState } from "react";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  // const [er, setEr] = useState();
+  // console.log(er);
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   return (
     <div>
@@ -45,6 +59,7 @@ const Login = () => {
                   <Link className="label-text-alt link link-hover">
                     Forgot password?
                   </Link>
+                  <br />
                 </label>
               </div>
               <div className="form-control mt-6">
--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,5 +1,9 @@
 import React, { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase";
 
 export const AuthContext = createContext();
@@ -7,7 +11,13 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
-  const authInfo = {};
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const authInfo = { createUser, signIn };
   return (
     <div>
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
